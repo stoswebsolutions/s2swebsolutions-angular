4a8c339851bd974fc820fa7831973f35f761b53a
manogna9866 <manogna.vita@gmail.com>
2026-04-10 12:57:00 +0530

    section=1,2,3,4,5,redesigned

--- src/app/header/home/section4/section4.ts
+++ src/app/header/home/section4/section4.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-section4',
+  standalone: true,
   imports: [RouterLink],
   templateUrl: './section4.html',
-  styleUrl: './section4.css',
+  styleUrls: ['./section4.css'],
 })
-export class Section4 {
-   
+export class Section4 implements AfterViewInit {
+  ngAfterViewInit(): void {
+    const accordionCards = document.querySelectorAll('.accordion-card');
+
+    accordionCards.forEach(card => {
+      const top = card.querySelector('.accordion-top');
+
+      top?.addEventListener('click', () => {
+        if (card.classList.contains('active')) {
+          card.classList.remove('active');
+        } else {
+          accordionCards.forEach(item => item.classList.remove('active'));
+          card.classList.add('active');
+        }
+      });
+    });
+  }
 }
